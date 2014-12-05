fc6ad1fdf2efc1e3b4c15ae46dac2379bad6724b
chris-pac <chris@Chriss-MacBook-Pro.local>
2014-12-05 14:37:18 -0500

    Added jinja handling and templates
    Removed html code from bore.py and added jinja templates

--- bora.py
+++ bora.py
@@ -1,89 +1,25 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
+# Created by Chris Pac on 12/4/14.
+# Copyright (c) 2014 Chris Pac. All rights reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
+# [START imports]
 import webapp2
 from google.appengine.api import users
 from google.appengine.ext import ndb
 import cgi
+import os
 import urllib
+import jinja2
 
-QUESTIONCREATE_PAGE_HTML = """\
-<html>
-  <body>
-    <form action="/question" method="post">
-      <div><p>What is your question?</p></div>
-      <div><textarea autofocus required name=content rows=20 cols=80></textarea></div>
-       <div><input type="submit" value="Create"></div>
-    </form>
-  </body>
-</html>
-"""
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
 
-
-QUESTIONDISPLAY_HTML = """\
-<div style="width:500px;border-style:ridge;padding: 5">
-<p>%s</p>
-<a href="/view/%s">View</a>
-</div>
-"""
-
-QUESTIONDISPLAY_VOTE_HTML = """\
-<html><body>
-<a href=/>Back to Questions</a><br></br>
-<div style="width:500px;border-style:ridge;padding: 5">
-<div>
-    <p style="float:left"><b>%s</b></p>
-    <p style="float:right">%s
-        <a href=/vote/up/%s><input type=button value='Up'></input></a>
-        <a href=/vote/down/%s><input type=button value='Down'></input></a>
-    </p>
-</div>
-<div style="clear:both"></div>
-<hr style="border-style:solid">
-"""
-ANSWERDISPLAY_VOTE_HTML = """\
-<div>
-    <p style="float:left">%s</p>
-    <p style="float:right">%s
-        <a href=/vote/up/%s><input type=button value='Up'></input></a>
-        <a href=/vote/down/%s><input type=button value='Down'></input></a>
-    </p>
-</div>
-<div style="clear:both"></div>
-<hr style="border-style:dashed">
-"""
-QUESTIONDISPLAY_VOTE_FOOTER_HTML = """\
-<div>
-    <form action=/answer method=post>
-        <p>What is your answer?</p>
-        <textarea required name=content rows=20 cols=68></textarea>
-        <input type=submit value=Submit></input>
-        <input type=hidden name=question value=%s></input>
-    </form>
-</div>
-</div></body></html>
-"""
-
-MAIN_PAGE_FOOTER_TEMPLATE = """\
-    <p>Hello %s</p>
-    <a href="%s">%s</a>
-  </body>
-</html>
-"""
-
+# [START models]
 class Vote(ndb.Model):
     author = ndb.UserProperty(required=True)
     updown = ndb.IntegerProperty(required=True)
@@ -105,55 +41,73 @@ class Answer(ndb.Model):
     modifydate = ndb.DateTimeProperty(auto_now=True)
     votes = ndb.StructuredProperty(Vote, repeated=True)
     score = ndb.IntegerProperty(default=0)    
-
+# [END models]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        self.response.write('<html><body>')
-        nickname = "Guest"
+        questions_query = Question.query().order(-Question.modifydate)
+        questions = questions_query.fetch(10)
+
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+            nickname = user.nickname()
+            user_ok = True
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+            nickname = 'Guest'
+            user_ok = False
+        
+        template_values = {
+            'questions': questions,
+            'user_ok': user_ok,
+            'user_nickname': nickname,
+            'user_url': url,
+            'user_url_linktext': url_linktext
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('main.html')
+        self.response.write(template.render(template_values))
+
+class QuestionView(webapp2.RequestHandler):
+    def get(self, question_link):
+        user = users.get_current_user()
+        question_key = ndb.Key(urlsafe=question_link)
+        
+        question = question_key.get()
+        answers_query = Answer.query(ancestor=question.key)
+        answers = answers_query.fetch(10)
         
         if user:
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
             nickname = user.nickname()
+            user_ok = True
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
+            nickname = 'Guest'
+            user_ok = False
         
-        questions_query = Question.query().order(-Question.modifydate)
+        template_values = {
+            'question': question,
+            'answers': answers,
+            'user_ok': user_ok,
+            'user_nickname': nickname,
+            'user_url': url,
+            'user_url_linktext': url_linktext
+        }
         
-        questions = questions_query.fetch(10)
-        
-        for question in questions:
-            self.response.write(QUESTIONDISPLAY_HTML % (question.shortcontent, question.key.urlsafe()))        
-        
-        
-        self.response.write('<div><a href=/question>Add Question</a></div>')
-        
-        self.response.write(MAIN_PAGE_FOOTER_TEMPLATE % (nickname, url, url_linktext))
-
-class QuestionView(webapp2.RequestHandler):
-    def get(self, question_link):
-        question_key = ndb.Key(urlsafe=question_link)
-        
-        question = question_key.get()
-        
-        
-        self.response.write(QUESTIONDISPLAY_VOTE_HTML % (question.content, question.score, question.key.urlsafe(), question.key.urlsafe()))
-        
-        answers_query = Answer.query(ancestor=question.key)
-        answers = answers_query.fetch(10)
-        for answer in answers:
-            self.response.write(ANSWERDISPLAY_VOTE_HTML % (answer.content, answer.score, answer.key.urlsafe(), answer.key.urlsafe()))
-            
-        
-        self.response.write(QUESTIONDISPLAY_VOTE_FOOTER_HTML % question.key.urlsafe())
+        template = JINJA_ENVIRONMENT.get_template('questionView.html')
+        self.response.write(template.render(template_values))
 
 
 class QuestionHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(QUESTIONCREATE_PAGE_HTML)
+        template = JINJA_ENVIRONMENT.get_template('questionCreate.html')
+        self.response.write(template.render())
         
     def post(self):
         user = users.get_current_user()
@@ -182,7 +136,7 @@ class AnswerHandler(webapp2.RequestHandler):
         else:
             self.redirect(users.create_login_url(self.request.uri))
         
-        self.redirect('/view?question=' + question_link)
+        self.redirect('/view/' + question_link)
 
 class VoteHandler(webapp2.RequestHandler):
     def get(self, updown, entity_link):
